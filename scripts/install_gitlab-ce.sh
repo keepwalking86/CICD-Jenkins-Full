73c20d31e0cc23124f11598f67bae8e0c5e46480
KeepWalking <dungnv@ditech.vn>
2019-04-01 08:51:06 +0700

    Update

--- scripts/install_gitlab-ce.sh
+++ scripts/install_gitlab-ce.sh
@@ -2,29 +2,37 @@
 #KeepWalking86
 #Installing GitLab-CE on Ubuntu/CentOS
 DOMAIN_GITLAB=gitlab.example.local
+
+#Check user account to run script
+if [ $UID -ne 0 ]; then
+        echo "You need root account to run script"
+        exit 1;
+fi
+
+#Check OS and install
 if [ -f /etc/debian_version ]; then
     #Installing required packages and open http/https on firewall
-    sudo apt-get update
-    sudo apt-get install -y curl openssh-server ca-certificates
-    sudo ufw allow http
-    sudo ufw allow https
+    apt-get update
+    apt-get install -y curl openssh-server ca-certificates
+    ufw allow http
+    ufw allow https
     #Add gitlab repo on Ubuntu
-    curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+    curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
     #URL for GitLab
-    sudo EXTERNAL_URL="http://${DOMAIN_GITLAB}" apt-get install gitlab-ce
+    EXTERNAL_URL="http://${DOMAIN_GITLAB}" apt-get install gitlab-ce
 else
     if [ -f /etc/redhat-release ]; then
         #Installing required packages and open http/https on firewall
-        sudo yum install -y curl policycoreutils-python openssh-server
-        sudo systemctl enable sshd
-        sudo systemctl start sshd
-        sudo firewall-cmd --permanent --add-service=http
-        sudo firewall-cmd --permanent --add-service=https
-        sudo systemctl reload firewalld
+        yum install -y curl policycoreutils-python openssh-server
+        systemctl enable sshd
+        systemctl start sshd
+        firewall-cmd --permanent --add-service=http
+        firewall-cmd --permanent --add-service=https
+        systemctl reload firewalld
         #Add gitlab repo on CentOS/RedHat
-        curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+        curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | bash
         #URL for GitLab
-        sudo EXTERNAL_URL="http://${DOMAIN_GITLAB}" yum install -y gitlab-ce
+        EXTERNAL_URL="http://${DOMAIN_GITLAB}" yum install -y gitlab-ce
     else
         echo "Distro hasn't been supported by this script"
         exit 1;
@@ -32,7 +40,8 @@ else
 fi
 
 #Setting mail
-#Replacing your mail setting informations
+echo "$(tput setaf 3)Replacing your mail setting informations$(tput sgr0)"
+sleep 4
 cat >>/etc/gitlab/gitlab.rb <<EOF
  gitlab_rails['smtp_enable'] = true
  gitlab_rails['smtp_address'] = "mail.vnsys.wordpress.com"
@@ -50,4 +59,4 @@ gitlab-ctl reconfigure
 
 ### Access to GitLab Web Interface
 IP_ADDR=` ip route get 1.1.1.1 | grep -oP 'src \K\S+'`
-echo -e "Access to GitLab Web Interface: http://$IP_ADDR
+echo -e "$(tput setaf 2)Access to GitLab Web Interface: http://$IP_ADDR$(tput sgr0)"
